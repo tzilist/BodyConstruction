48c5e31a7f5f27f1369b4f21fda06b8c16e86da9
Gregory Grubbs <gregory@dynapse.com>
2015-10-27 03:55:58 +0000

    In header.html: fix img path to match renamed images dir In views.py: use render_template in index()

--- BodyConstruction/views.py
+++ BodyConstruction/views.py
@@ -1,5 +1,11 @@
 from BodyConstruction import app
+from flask import render_template
 
 @app.route('/')
+@app.route('/index')
+
 def index():
+    return render_template('header.html',
+                           title='Gort Gort')
+        
     return 'Hello World!'
