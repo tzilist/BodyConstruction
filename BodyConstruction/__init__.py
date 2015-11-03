17319ab7c2a9a85e6b817287ed8c871b8924b7b4
Gregory Grubbs <gregory@dynapse.com>
2015-11-03 15:13:33 +0000

    Prepare for DB session in app

--- BodyConstruction/__init__.py
+++ BodyConstruction/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 import BodyConstruction.views
+
+## deal with DB - is this best practice?
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://gregj:dbuser@localhost/bodyconstruction'
+db = SQLAlchemy(app)
+
+metadata = db.metadata
+engine = db.engine
+
+## grab metadata for all tables!
+metadata.reflect(bind = engine)
+user = metadata.tables['user']
+journal = metadata.tables['journal']
