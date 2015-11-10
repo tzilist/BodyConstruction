4ee107b6e8a17f622bf53532693de6e0ae0b2354
Gregory Grubbs <gregory@dynapse.com>
2015-11-09 17:20:49 -0700

    add db init

--- BodyConstruction/__init__.py
+++ BodyConstruction/__init__.py
@@ -1,18 +1,20 @@
+"""BodyConstruction web application for keeping track of body building progress"""
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-import BodyConstruction.views
 
 ## deal with DB - is this best practice?
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://gregj:dbuser@localhost/bodyconstruction'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
 db = SQLAlchemy(app)
 
-metadata = db.metadata
-engine = db.engine
-
 ## grab metadata for all tables!
-metadata.reflect(bind = engine)
-user = metadata.tables['user']
-journal = metadata.tables['journal']
+db.metadata.reflect(bind = db.engine)
+user_id = -1
+
+import BodyConstruction.views
+
+__version__ = "0.0.0"
